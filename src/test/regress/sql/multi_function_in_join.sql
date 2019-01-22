b361098dd45d5481def39d2bdb801ede37f56931
Hanefi Onaldi <hanefi@citusdata.com>
2019-01-22 14:11:50 +0300

    Support RowType functions in joins

--- src/test/regress/sql/multi_function_in_join.sql
+++ src/test/regress/sql/multi_function_in_join.sql
@@ -22,17 +22,17 @@ SELECT x, x*x
 from generate_series(1, 100) as f (x);
 
 -- Verbose messages for observing the subqueries that wrapped function calls
-SET client_min_messages TO DEBUG;
+SET client_min_messages TO DEBUG1;
 
 -- Check joins on a sequence
 CREATE SEQUENCE numbers;
-SELECT * FROM table1 JOIN nextval('numbers') n ON (id = n);
+SELECT * FROM table1 JOIN nextval('numbers') n ON (id = n) ORDER BY id ASC;
 
 -- Check joins of a function that returns a single integer
 CREATE FUNCTION add(integer, integer) RETURNS integer
 AS 'SELECT $1 + $2;'
 LANGUAGE SQL;
-SELECT * FROM table1 JOIN add(3,5) sum ON (id = sum);
+SELECT * FROM table1 JOIN add(3,5) sum ON (id = sum) ORDER BY id ASC;
 
 -- Check join of plpgsql functions
 -- a function returning a single integer
@@ -41,7 +41,7 @@ BEGIN
   RETURN i + 1;
 END;
 $$ LANGUAGE plpgsql;
-SELECT * FROM table1 JOIN increment(2) val ON (id = val);
+SELECT * FROM table1 JOIN increment(2) val ON (id = val) ORDER BY id ASC;
 
 -- a function that returns a set of integers
 CREATE OR REPLACE FUNCTION next_k_integers(IN first_value INTEGER,
@@ -56,13 +56,36 @@ SELECT *
 FROM table1 JOIN next_k_integers(3,2) next_integers ON (id = next_integers.result)
 ORDER BY id ASC;
 
+-- a function returning set of records
+CREATE FUNCTION get_set_of_records() RETURNS SETOF RECORD AS $cmd$
+SELECT x, x+1 FROM generate_series(0,4) f(x)
+$cmd$
+LANGUAGE SQL;
+SELECT * FROM table1 JOIN get_set_of_records() AS t2(x int, y int) ON (id = x) ORDER BY id ASC;
+
+-- a function returning table
+CREATE FUNCTION dup(int) RETURNS TABLE(f1 int, f2 text)
+AS $$ SELECT $1, CAST($1 AS text) || ' is text' $$
+LANGUAGE SQL;
+
+SELECT dup(42);
+SELECT * FROM dup(42);
+SELECT f.* FROM dup(42) f;
+
+SELECT f.* FROM table1 t JOIN dup(32) f ON (f1 = id);
+SELECT f.* FROM table1 t JOIN (SELECT f_1.* FROM dup(32) f_1) f ON (f1 = id);
+
+SELECT *, f.* from generate_series(1,3) g(x) JOIN LATERAL dup(x) f ON (f1 = x);
+SELECT * from generate_series(1,3) g(x) JOIN LATERAL dup(x) f ON (f1 = x);
+SELECT *, d.* FROM table1 JOIN dup(42) d ON (id = f1);
+
 -- a stable function
 CREATE OR REPLACE FUNCTION the_minimum_id()
   RETURNS INTEGER STABLE AS 'SELECT min(id) FROM table1' LANGUAGE SQL;
 SELECT * FROM table1 JOIN the_minimum_id() min_id ON (id = min_id);
 
 -- a built-in immutable function
-SELECT * FROM table1 JOIN abs(100) as hundred ON (id = hundred);
+SELECT * FROM table1 JOIN abs(100) as hundred ON (id = hundred) ORDER BY id ASC;
 
 -- function joins inside a CTE
 WITH next_row_to_process AS (
@@ -70,7 +93,12 @@ WITH next_row_to_process AS (
     )
 SELECT *
 FROM table1, next_row_to_process
-WHERE table1.data <= next_row_to_process.data;
+WHERE table1.data <= next_row_to_process.data
+ORDER BY id ASC;
+
+-- Multiple functions in an RTE
+SELECT * FROM ROWS FROM (next_k_integers(5), next_k_integers(10)) AS f(a, b),
+    table1 WHERE id = a ORDER BY id ASC;
 
 
 -- The following tests will fail as we do not support  all joins on
@@ -87,24 +115,6 @@ FROM one_row, table1, next_k_integers(one_row.id, 5) next_five_ids
 WHERE table1.id = next_five_ids;
 $cmd$);
 
--- a function returning table
-CREATE FUNCTION get_two_column_table() RETURNS TABLE(x int,y int) AS
-$cmd$
-SELECT x, x+1 FROM generate_series(0,4) f(x)
-$cmd$
-LANGUAGE SQL;
-SELECT public.raise_failed_execution($cmd$
-SELECT * FROM table1 JOIN get_two_column_table() t2 ON (id = x)
-$cmd$);
-
--- a function returning records
-CREATE FUNCTION get_set_of_records() RETURNS SETOF RECORD AS $cmd$
-SELECT x, x+1 FROM generate_series(0,4) f(x)
-$cmd$
-LANGUAGE SQL;
-SELECT public.raise_failed_execution($cmd$
-SELECT * FROM table1 JOIN get_set_of_records() AS t2(x int, y int) ON (id = x)
-$cmd$);
 
 -- a user-defined immutable function
 CREATE OR REPLACE FUNCTION the_answer_to_life()
@@ -112,12 +122,6 @@ CREATE OR REPLACE FUNCTION the_answer_to_life()
 SELECT public.raise_failed_execution($cmd$
 SELECT * FROM table1 JOIN the_answer_to_life() the_answer ON (id = the_answer)
 $cmd$);
-
--- Multiple functions in an RTE
--- NOTE: Wrapping the functions in subqueries does not work as postgres does
--- not allow having a subquery after a ROWS FROM clause.
-SELECT * FROM ROWS FROM (next_k_integers(5), next_k_integers(10)) AS f(a, b),
-    table1 WHERE id = a;
 
 -- WITH ORDINALITY clause forcing the result type to be RECORD/RECORDs
 SELECT *
